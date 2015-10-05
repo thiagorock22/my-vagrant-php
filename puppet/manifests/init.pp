7f515611ee782b4997cd695a5584025429c0e584
Thiago <thiagorock22@gmail.com>
2015-10-05 11:15:42 -0300

    added mysql

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -10,7 +10,31 @@ package { 'htop':
     ensure => present,
 }
 
-
 package { 'language-pack-pt':
     ensure => present,
+}
+
+class { 'apache':
+  default_vhost => false,
+}
+
+apache::vhost { 'devlocal':
+  port    => '80',
+  docroot => '/var/www',
+  docroot_owner => 'www-data',
+  docroot_group => 'www-data',
+}
+
+apache::vhost { 'subdomain.devlocal':
+  vhost_name       => '*',
+  port             => '80',
+  virtual_docroot  => '/var/www/%-2+',
+  docroot          => '/var/www',
+  serveraliases    => ['*.devlocal',],
+}
+
+class { 'apache::mod::rewrite': }
+
+class { 'mysql::server':
+  root_password => '123456',
 }
